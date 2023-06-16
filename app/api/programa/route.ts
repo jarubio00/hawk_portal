18c4c60f6a7ef90020d725cb70da39a445e4b02c
jarubio00 <jarubio@xinet.com.mx>
2023-06-16 12:49:34 -0600

    xinet2

--- app/api/programa/route.ts
+++ app/api/programa/route.ts
@@ -6,7 +6,10 @@ export async function GET(
   request: Request, 
 ) {
 
+  console.log('api fecha')
   const serverDate = new Date();
+
+  console.log(serverDate);
 
   return NextResponse.json(serverDate);
 }
