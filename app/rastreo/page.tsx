7107cdcd7c98538a9c019edae92add8f54e8f71b
jarubio00 <jarubio@xinet.com.mx>
2023-07-12 21:09:53 -0600

    xin

--- app/rastreo/page.tsx
+++ app/rastreo/page.tsx
@@ -1,10 +1,32 @@
 
 import Head from 'next/head';
+import { Metadata } from 'next'
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "../components/ClientOnly";
 import Container from "@/app/components/Container";
 import Navbar from "../components/navbar/Navbar";
 import MainDrawer from '../components/drawer/MainDrawer';
+
+
+export const metadata: Metadata = {
+  title: 'Rastrea tu envío',
+  description: 'Rastrea en tiempo real tu envío',
+  openGraph: {
+    title: 'La Mensajería MX',
+    description: 'Aplicación de rastreo de envíos',
+    url: 'https://hawkportal.lamensajeria.mx/rastreo',
+    siteName: 'Hawk',
+    images: [
+      {
+        url: 'https://firebasestorage.googleapis.com/v0/b/hawk-admin.appspot.com/o/webimages%2FogLm.png?alt=media&token=677649ae-a87f-47b1-abf2-64435b998223',
+        width: 400,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+}
 
 interface HomeProps {
   searchParams: any
@@ -18,17 +40,6 @@ const Home = async ({ searchParams }: HomeProps) => {
 
   return (
     <ClientOnly>
-      <Head>
-        <meta property="og:title" content="Social Title for Cool Page" />
-        <meta
-          property="og:description"
-          content="And a social description for our cool page"
-        />
-        <meta
-          property="og:image"
-          content="https://firebasestorage.googleapis.com/v0/b/hawk-admin.appspot.com/o/webimages%2FogLm.png?alt=media&token=677649ae-a87f-47b1-abf2-64435b998223"
-        />
-      </Head>
       <MainDrawer />
       <Navbar currentUser={currentUser}/>
       
