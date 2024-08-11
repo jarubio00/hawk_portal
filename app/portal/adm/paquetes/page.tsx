1c50b25ee3d3f59f2065dded563cf7688a3ef52c
jarubio00 <jarubio@xinet.com.mx>
2024-08-11 16:51:31 -0600

    xin

--- app/portal/adm/paquetes/page.tsx
+++ app/portal/adm/paquetes/page.tsx
@@ -1,28 +1,20 @@
-
 import ClientOnly from "@/app/components/ClientOnly";
 
 import getDirecciones from "@/app/actions/getDirecciones";
-import getCurrentUser from '@/app/actions/getCurrentUser';
-import getPaquetes from '@/app/actions/getPaquetes';
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getPaquetes from "@/app/actions/getPaquetes";
 
 import PaquetesClient from "./Paquetes";
 
-
-const Paquetes  = async () => {
-
-
-  const direcciones = await getDirecciones();
+const Paquetes = async () => {
   const currentUser = await getCurrentUser();
-  const paquetes = await getPaquetes();
+  //const paquetes = await getPaquetes();
 
+  return (
+    <ClientOnly>
+      <PaquetesClient data={[]} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
 
-    return (
-      <ClientOnly>
-        <PaquetesClient  data={paquetes} currentUser={currentUser}/>
-
-      </ClientOnly>
-    )
-  }
-
-  
 export default Paquetes;
