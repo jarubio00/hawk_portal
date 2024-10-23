ce1c11c6f64475c7dd09271bbbe6dde43ae628e5
jarubio00 <jarubio@xinet.com.mx>
2024-10-23 17:15:29 -0600

    xin 2.1.20

--- version.js
+++ version.js
@@ -1,6 +1,11 @@
-export const appVersion = "2.1.19";
+export const appVersion = "2.1.20";
 
 /* 
+2.1.20
+23/10/24 17:14
+
+Se corrigio el NEXTAUTH_URL de localhost a hawkportal
+
 2.1.19
 Correccion de NexResponse.error() en api routes, Cambio de version de .env a version.js
 Desactivar console.log app/api/*
